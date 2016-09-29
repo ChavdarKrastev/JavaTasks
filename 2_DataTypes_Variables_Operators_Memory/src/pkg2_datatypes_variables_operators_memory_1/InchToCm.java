e4428583d53665c50557693c5f2edc005ffb5798
user1 <user1@VAIO>
2016-09-30 00:47:37 +0300

    Lecture 2, all work

---
+++ 2_DataTypes_Variables_Operators_Memory/src/pkg2_datatypes_variables_operators_memory_1/InchToCm.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg2_datatypes_variables_operators_memory_1;
+
+/**
+ *
+ * @author user1
+ */
+public class InchToCm {
+     public static void main(String[] args)
+    {
+    double inch = 2;
+    double cm = inch*2.54;
+
+    System.out.println(cm);
+    }
+    
+}
