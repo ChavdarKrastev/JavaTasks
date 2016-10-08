e1dd02acfb1c8e92c8d8d30815cbf178c9664677
user1 <user1@VAIO>
2016-10-09 01:23:47 +0300

    Task 1 working, tested

--- 6_OOPPrinciples_Debugging/src/CredentialsManager/Main.java
+++ 6_OOPPrinciples_Debugging/src/CredentialsManager/Main.java
@@ -8,15 +8,18 @@ public class Main {
 
         Scanner sc = new Scanner(System.in);
 
-        String command = sc.nextLine();
-        String user;
-        String password;
+        String str = sc.nextLine();
+        String[] splittedBySpace;
+        splittedBySpace = str.split(" ");
 
-        while (command != "END") {
+        String command = splittedBySpace[0];
+        String user = splittedBySpace[1];
+        String password = splittedBySpace[2];
+        String newPassword = null;
+
+        while (!command.equals("END")) {
             switch (command) {
                 case "ENROLL":
-                    user = sc.next();
-                    password = sc.next();
                     if (!Credentials.userExist(user)) {
                         Credentials User = new Credentials(user, password);
                         Credentials.addObject(User);
@@ -28,9 +31,6 @@ public class Main {
                         break;
                     }
                 case "CHPASS":
-                    user = sc.next();
-                    password = sc.next();
-                    String newPassword = sc.next();
                     if (Credentials.userExist(user)) {
                         if (Credentials.findObject(user).checkPassword(password)
                                 && (!Credentials.findObject(user).isPasswordUsed(newPassword))) {
@@ -47,19 +47,31 @@ public class Main {
                         break;
                     }
                 case "AUTH":
-                    user = sc.next();
-                    password = sc.next();
                     if (Credentials.findObject(user).checkPassword(password)
                             && Credentials.userExist(user)) {
-                        System.out.println("CHPASS success");
+                        System.out.println("AUTH success");
                         break;
                     } else {
-                        System.out.println("CHPASS fail");
+                        System.out.println("AUTH fail");
                         break;
                     }
-
             }
-            command = sc.nextLine();
+            str = sc.nextLine();
+            splittedBySpace = str.split(" ");
+            if (splittedBySpace.length == 4) {
+                command = splittedBySpace[0];
+                user = splittedBySpace[1];
+                password = splittedBySpace[2];
+                newPassword = splittedBySpace[3];
+            }
+            if (splittedBySpace.length == 3) {
+                command = splittedBySpace[0];
+                user = splittedBySpace[1];
+                password = splittedBySpace[2];
+            }
+            if (splittedBySpace.length == 1) {
+                command = splittedBySpace[0];
+            }
         }
     }
 }
